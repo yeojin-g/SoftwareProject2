0f3a422a31800f35258a96edba41c99dd87c661a
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2021-10-07 12:20:44 +0900

    Make recbinsearch() with a recursive function.
    Make recbinsearch() with a recursive function.

---
+++ BinarySearch_yeojin.py
@@ -0,0 +1,57 @@
+import time
+import random
+
+
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
+
+
+def recbinsearch(L, l, u, target):
+    middle = int((l + u) // 2)
+    if L[middle] == target:
+        return middle
+    elif L[middle] < target:
+        l = middle + 1
+    else:
+        u = middle - 1
+    if l > u : return -1
+    return recbinsearch(L, l, u, target)
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers)+1, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
