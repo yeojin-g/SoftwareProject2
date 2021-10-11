1d65aa6de7b1127bdbc5396a2e0fb8576ca176aa
권상혁 <60140454+ksg222@users.noreply.github.com>
2021-10-11 19:11:40 +0900

    Add files via upload

---
+++ BinarySearch-Sanyo.py
@@ -0,0 +1,59 @@
+import time
+import random
+
+
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
+
+
+def recbinsearch(L, l, u, target):
+    middle_idx = ((u-l) // 2) + l
+    if(l == u-1):
+        return -1
+    if L[middle_idx] == target:
+        return middle_idx
+    elif L[middle_idx] < target:
+        l = middle_idx + 1
+    else:
+        u = middle_idx
+
+    return recbinsearch(L, l, u, target)
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
---
+++ fibo-Sanyo.py
@@ -0,0 +1,28 @@
+import time
+
+
+def fibo_iter(n):
+    num1, num2 = 0, 1
+    answer = 1
+    for i in range(n-1):
+        answer = num1 + num2
+        num1, num2 = num2, answer
+    return answer
+
+def fibo_rec(n):
+    if n <= 1:
+        return n
+    return fibo_rec(n - 1) + fibo_rec(n - 2)
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = fibo_rec(nbr)
+    ts = time.time() - ts
+    print("Fibo_rec(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+
+    ts = time.time()
+    fibonumber = fibo_iter(nbr)
+    ts = time.time() - ts
+    print("Fibo_iter(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
