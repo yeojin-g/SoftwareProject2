beebf1fb7fff7c3db0a18a5511f981a6c628a0b8
kds20212962 <84118154+kds20212962@users.noreply.github.com>
2021-10-11 15:55:35 +0900

    Create BinarySearch_20212962.py

---
+++ BinarySearch_20212962.py
@@ -0,0 +1,56 @@
+import time
+import random
+
+
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
+
+
+def recbinsearch(L, l, u, target):
+    while L[len(L)//2] == target:
+        if L[len(L)//2] == target:
+            return L[len(L)//2]
+        elif L[len(L)//2] < target:
+            return recbinsearch(L, L[len(L)//2]+1, u, target)
+        else:
+            return recbinsearch(L, l, L[len(L)//2]-1, target)
+    return -1
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
