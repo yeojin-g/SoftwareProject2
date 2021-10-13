3c661954f80c7c56669999963e4bcab9e89d704d
권상혁 <60140454+ksg222@users.noreply.github.com>
2021-10-13 19:35:03 +0900

    change def deletePersonalData to fix issue

--- BinarySearch-Sanyo.py
+++ BinarySearch-Sanyo.py
@@ -8,7 +8,6 @@ def seqsearch(nbrs, target):
             return i
     return -1
 
-
 def recbinsearch(L, l, u, target):
     middle_idx = ((u-l) // 2) + l
     if(l == u-1):
@@ -25,6 +24,7 @@ def recbinsearch(L, l, u, target):
 
 numofnbrs = int(input("Enter a number: "))
 numbers = []
+
 for i in range(numofnbrs):
     numbers += [random.randint(0, 999999)]
 
@@ -35,20 +35,19 @@ targets = []
 for i in range(numoftargets):
     targets += [random.randint(0, 999999)]
 
-
 ts = time.time()
-
 # binary search - recursive
 cnt = 0
+idx = 0
 for target in targets:
     idx = recbinsearch(numbers, 0, len(numbers), target)
     if idx == -1:
         cnt += 1
 ts = time.time() - ts
 print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+print(idx)
 
 ts = time.time()
-
 # sequential search
 cnt = 0
 for target in targets:
