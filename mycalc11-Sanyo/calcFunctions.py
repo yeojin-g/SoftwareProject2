466603c9eb6be1dde4a78da50cc1aafb655da418
권상혁 <60140454+ksg222@users.noreply.github.com>
2021-11-14 17:59:14 +0900

    Update calcFunctions.py

--- mycalc11-Sanyo/calcFunctions.py
+++ mycalc11-Sanyo/calcFunctions.py
@@ -37,22 +37,47 @@ def decToRoman(numStr):
     if n >= 4000:
         return 'Error!'
 
-    romans = [
-        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
-        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
-        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
-        (1, 'I')
-    ]
+    romans = {
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+        100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+        10: 'X', 9: 'IX', 5: 'V', 4: 'IV',
+        1: 'I'
+    }
 
     result = ''
-    for value, letters in romans:
+    for value in sorted(romans.keys(), reverse=True):
         while n >= value:
-            result += letters
+            result += romans[value]
             n -= value
 
     return result
 
 
 def romanToDec(numStr):
-    return 'Roman to Dec'
+
+    romans = {
+        'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
+        'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+        'X': 10, 'IX': 9, 'V': 5, 'IV': 4,
+        'I': 1
+    }
+
+    n = 0
+    temp = ''
+    for letter in numStr:
+        temp += letter
+        if len(temp) == 2:
+            if temp in romans.keys():
+                n += romans[temp]
+                temp = ''
+            else:
+                try:
+                    n += romans[temp[0]]
+                    temp = temp[1:]
+                except:
+                    n = 'Error'
+                    break
+    if temp in romans.keys():
+        n += romans[temp]
+    return n
 
